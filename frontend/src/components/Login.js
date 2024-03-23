7630299495bdbaf8f009a877c77a37b51fec382d
Abhishek-Paspunuri <abhishek.paspunuri@gmail.com>
2024-03-24 01:39:10 +0530

    changed to onrender urls

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`https://vercel-effortles-ops.vercel.app/api/user/login`, {
+    const response = await fetch(`https://effortlessops-backend.onrender.com/api/user/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
